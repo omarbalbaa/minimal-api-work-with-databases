a0889c3b4870f9cd3f668f3a0410db9da6f9db1e
Omar Balbaa <69731891+omarbalbaa@users.noreply.github.com>
2025-11-11 02:54:51 +0000

    build frontend

---
+++ PizzaClient/vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,  // Client port
+    proxy: {
+      '/pizzas': {
+        target: 'https://supreme-chainsaw-p969g4gp47rh7676-5100.app.github.dev/', // Mock server port
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      }
+    }
+  }
+})
+
